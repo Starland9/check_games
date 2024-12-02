35a49425158cd497f74349ce0e8a09b2bc14341a
Landry Simo <landrysimo99@gmail.com>
2024-12-02 15:27:17 +0100

    Adds assets for game buttons
    Adds 46 new button image assets to the project.  These assets are organized into a new `assets/images/buttons` directory and are accessible via the generated `assets.gen.dart` file.

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -26,6 +26,13 @@ class $AssetsIconGen {
   List<AssetGenImage> get values => [cartomanioLogo, logo];
 }
 
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/buttons
+  $AssetsImagesButtonsGen get buttons => const $AssetsImagesButtonsGen();
+}
+
 class $AssetsOthersGen {
   const $AssetsOthersGen();
 
@@ -33,14 +40,257 @@ class $AssetsOthersGen {
   SvgGenImage get casinoCardsSvgrepoCom =>
       const SvgGenImage('assets/others/casino-cards-svgrepo-com.svg');
 
+  /// File path: assets/others/poker_bg.jpg
+  AssetGenImage get pokerBg =>
+      const AssetGenImage('assets/others/poker_bg.jpg');
+
   /// List of all assets
-  List<SvgGenImage> get values => [casinoCardsSvgrepoCom];
+  List<dynamic> get values => [casinoCardsSvgrepoCom, pokerBg];
+}
+
+class $AssetsImagesButtonsGen {
+  const $AssetsImagesButtonsGen();
+
+  /// File path: assets/images/buttons/free_button_01.png
+  AssetGenImage get freeButton01 =>
+      const AssetGenImage('assets/images/buttons/free_button_01.png');
+
+  /// File path: assets/images/buttons/free_button_02.png
+  AssetGenImage get freeButton02 =>
+      const AssetGenImage('assets/images/buttons/free_button_02.png');
+
+  /// File path: assets/images/buttons/free_button_03.png
+  AssetGenImage get freeButton03 =>
+      const AssetGenImage('assets/images/buttons/free_button_03.png');
+
+  /// File path: assets/images/buttons/free_button_04.png
+  AssetGenImage get freeButton04 =>
+      const AssetGenImage('assets/images/buttons/free_button_04.png');
+
+  /// File path: assets/images/buttons/free_button_05.png
+  AssetGenImage get freeButton05 =>
+      const AssetGenImage('assets/images/buttons/free_button_05.png');
+
+  /// File path: assets/images/buttons/free_button_06.png
+  AssetGenImage get freeButton06 =>
+      const AssetGenImage('assets/images/buttons/free_button_06.png');
+
+  /// File path: assets/images/buttons/free_button_07.png
+  AssetGenImage get freeButton07 =>
+      const AssetGenImage('assets/images/buttons/free_button_07.png');
+
+  /// File path: assets/images/buttons/free_button_08.png
+  AssetGenImage get freeButton08 =>
+      const AssetGenImage('assets/images/buttons/free_button_08.png');
+
+  /// File path: assets/images/buttons/free_button_09.png
+  AssetGenImage get freeButton09 =>
+      const AssetGenImage('assets/images/buttons/free_button_09.png');
+
+  /// File path: assets/images/buttons/free_button_10.png
+  AssetGenImage get freeButton10 =>
+      const AssetGenImage('assets/images/buttons/free_button_10.png');
+
+  /// File path: assets/images/buttons/free_button_11.png
+  AssetGenImage get freeButton11 =>
+      const AssetGenImage('assets/images/buttons/free_button_11.png');
+
+  /// File path: assets/images/buttons/free_button_12.png
+  AssetGenImage get freeButton12 =>
+      const AssetGenImage('assets/images/buttons/free_button_12.png');
+
+  /// File path: assets/images/buttons/free_button_13.png
+  AssetGenImage get freeButton13 =>
+      const AssetGenImage('assets/images/buttons/free_button_13.png');
+
+  /// File path: assets/images/buttons/free_button_14.png
+  AssetGenImage get freeButton14 =>
+      const AssetGenImage('assets/images/buttons/free_button_14.png');
+
+  /// File path: assets/images/buttons/free_button_15.png
+  AssetGenImage get freeButton15 =>
+      const AssetGenImage('assets/images/buttons/free_button_15.png');
+
+  /// File path: assets/images/buttons/free_button_16.png
+  AssetGenImage get freeButton16 =>
+      const AssetGenImage('assets/images/buttons/free_button_16.png');
+
+  /// File path: assets/images/buttons/free_button_17.png
+  AssetGenImage get freeButton17 =>
+      const AssetGenImage('assets/images/buttons/free_button_17.png');
+
+  /// File path: assets/images/buttons/free_button_18.png
+  AssetGenImage get freeButton18 =>
+      const AssetGenImage('assets/images/buttons/free_button_18.png');
+
+  /// File path: assets/images/buttons/free_button_19.png
+  AssetGenImage get freeButton19 =>
+      const AssetGenImage('assets/images/buttons/free_button_19.png');
+
+  /// File path: assets/images/buttons/free_button_20.png
+  AssetGenImage get freeButton20 =>
+      const AssetGenImage('assets/images/buttons/free_button_20.png');
+
+  /// File path: assets/images/buttons/free_button_21.png
+  AssetGenImage get freeButton21 =>
+      const AssetGenImage('assets/images/buttons/free_button_21.png');
+
+  /// File path: assets/images/buttons/free_button_22.png
+  AssetGenImage get freeButton22 =>
+      const AssetGenImage('assets/images/buttons/free_button_22.png');
+
+  /// File path: assets/images/buttons/free_button_23.png
+  AssetGenImage get freeButton23 =>
+      const AssetGenImage('assets/images/buttons/free_button_23.png');
+
+  /// File path: assets/images/buttons/free_button_24.png
+  AssetGenImage get freeButton24 =>
+      const AssetGenImage('assets/images/buttons/free_button_24.png');
+
+  /// File path: assets/images/buttons/free_button_25.png
+  AssetGenImage get freeButton25 =>
+      const AssetGenImage('assets/images/buttons/free_button_25.png');
+
+  /// File path: assets/images/buttons/free_button_26.png
+  AssetGenImage get freeButton26 =>
+      const AssetGenImage('assets/images/buttons/free_button_26.png');
+
+  /// File path: assets/images/buttons/free_button_27.png
+  AssetGenImage get freeButton27 =>
+      const AssetGenImage('assets/images/buttons/free_button_27.png');
+
+  /// File path: assets/images/buttons/free_button_28.png
+  AssetGenImage get freeButton28 =>
+      const AssetGenImage('assets/images/buttons/free_button_28.png');
+
+  /// File path: assets/images/buttons/free_button_29.png
+  AssetGenImage get freeButton29 =>
+      const AssetGenImage('assets/images/buttons/free_button_29.png');
+
+  /// File path: assets/images/buttons/free_button_30.png
+  AssetGenImage get freeButton30 =>
+      const AssetGenImage('assets/images/buttons/free_button_30.png');
+
+  /// File path: assets/images/buttons/free_button_31.png
+  AssetGenImage get freeButton31 =>
+      const AssetGenImage('assets/images/buttons/free_button_31.png');
+
+  /// File path: assets/images/buttons/free_button_32.png
+  AssetGenImage get freeButton32 =>
+      const AssetGenImage('assets/images/buttons/free_button_32.png');
+
+  /// File path: assets/images/buttons/free_button_33.png
+  AssetGenImage get freeButton33 =>
+      const AssetGenImage('assets/images/buttons/free_button_33.png');
+
+  /// File path: assets/images/buttons/free_button_34.png
+  AssetGenImage get freeButton34 =>
+      const AssetGenImage('assets/images/buttons/free_button_34.png');
+
+  /// File path: assets/images/buttons/free_button_35.png
+  AssetGenImage get freeButton35 =>
+      const AssetGenImage('assets/images/buttons/free_button_35.png');
+
+  /// File path: assets/images/buttons/free_button_36.png
+  AssetGenImage get freeButton36 =>
+      const AssetGenImage('assets/images/buttons/free_button_36.png');
+
+  /// File path: assets/images/buttons/free_button_37.png
+  AssetGenImage get freeButton37 =>
+      const AssetGenImage('assets/images/buttons/free_button_37.png');
+
+  /// File path: assets/images/buttons/free_button_38.png
+  AssetGenImage get freeButton38 =>
+      const AssetGenImage('assets/images/buttons/free_button_38.png');
+
+  /// File path: assets/images/buttons/free_button_39.png
+  AssetGenImage get freeButton39 =>
+      const AssetGenImage('assets/images/buttons/free_button_39.png');
+
+  /// File path: assets/images/buttons/free_button_40.png
+  AssetGenImage get freeButton40 =>
+      const AssetGenImage('assets/images/buttons/free_button_40.png');
+
+  /// File path: assets/images/buttons/free_button_41.png
+  AssetGenImage get freeButton41 =>
+      const AssetGenImage('assets/images/buttons/free_button_41.png');
+
+  /// File path: assets/images/buttons/free_button_42.png
+  AssetGenImage get freeButton42 =>
+      const AssetGenImage('assets/images/buttons/free_button_42.png');
+
+  /// File path: assets/images/buttons/free_button_43.png
+  AssetGenImage get freeButton43 =>
+      const AssetGenImage('assets/images/buttons/free_button_43.png');
+
+  /// File path: assets/images/buttons/free_button_44.png
+  AssetGenImage get freeButton44 =>
+      const AssetGenImage('assets/images/buttons/free_button_44.png');
+
+  /// File path: assets/images/buttons/free_button_45.png
+  AssetGenImage get freeButton45 =>
+      const AssetGenImage('assets/images/buttons/free_button_45.png');
+
+  /// File path: assets/images/buttons/free_button_46.png
+  AssetGenImage get freeButton46 =>
+      const AssetGenImage('assets/images/buttons/free_button_46.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        freeButton01,
+        freeButton02,
+        freeButton03,
+        freeButton04,
+        freeButton05,
+        freeButton06,
+        freeButton07,
+        freeButton08,
+        freeButton09,
+        freeButton10,
+        freeButton11,
+        freeButton12,
+        freeButton13,
+        freeButton14,
+        freeButton15,
+        freeButton16,
+        freeButton17,
+        freeButton18,
+        freeButton19,
+        freeButton20,
+        freeButton21,
+        freeButton22,
+        freeButton23,
+        freeButton24,
+        freeButton25,
+        freeButton26,
+        freeButton27,
+        freeButton28,
+        freeButton29,
+        freeButton30,
+        freeButton31,
+        freeButton32,
+        freeButton33,
+        freeButton34,
+        freeButton35,
+        freeButton36,
+        freeButton37,
+        freeButton38,
+        freeButton39,
+        freeButton40,
+        freeButton41,
+        freeButton42,
+        freeButton43,
+        freeButton44,
+        freeButton45,
+        freeButton46
+      ];
 }
 
 class Assets {
   Assets._();
 
   static const $AssetsIconGen icon = $AssetsIconGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsOthersGen others = $AssetsOthersGen();
 }
 
